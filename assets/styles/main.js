0bd5a8d32fecc9831c218f5244b137562b0e1ff3
Git Network <chilll.website.supp0rt@gmail.com>
2022-03-22 18:24:59 -0400

    Update main.js

--- assets/styles/main.js
+++ assets/styles/main.js
@@ -27,7 +27,7 @@ var baseUrl = getUrl.host;
 if (baseUrl.includes("github")) {
     document.getElementById('login').style = "display: none;"
 } else if (baseUrl.includes('localhost')) {
-    baseUrl = 'totallyscience.co'
+    baseUrl = 'loop-beta.vercel.app'
 }
 
 
@@ -45,9 +45,7 @@ window.addEventListener(
 )
 
 mkHtml(`
-        <svg id="scrollb" onclick='window.scrollTo({top: 0, behavior: "smooth"});' xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#f75dfc" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
-        </svg>`)
+        <svg id="scrollb" onclick='window.scrollTo({top: 0, behavior: "smooth"});' xmlns="/img/up.png" width="48" height="48" 
 const scrollButton = $('scrollb')
 
 if (scrollButton) {
